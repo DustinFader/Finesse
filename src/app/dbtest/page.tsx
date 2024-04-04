17f18f13ca63c3659dd3c6879d9f19ef3df5b650
M-ARTHELL <makoarthell@proton.me>
2024-04-04 16:54:43 -0300

    setup nextui

--- src/app/dbtest/page.tsx
+++ src/app/dbtest/page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { PrismaClient } from "@prisma/client";
+import { Button } from "@nextui-org/react";
 
 const prisma = new PrismaClient();
 
 async function main() {
   const allUsers = await prisma.users.findMany()
-  console.log(allUsers)
+  console.log (allUsers)
 }
 
 export default async function Test() {
@@ -22,6 +23,10 @@ export default async function Test() {
   return (
     <div>
       testing testing
+      <Button>
+        Button
+      </Button>
+      <p>testing testing</p>
     </div>
   );
 };
