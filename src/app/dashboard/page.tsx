fb20b07c2780e58638caf420df5384852b39ac82
DustinFader <dustinfader16@gmail.com>
2024-04-05 00:18:26 -0300

    add a dashboard page and auth file

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,84 @@
+'use client'
+
+import React from "react";
+
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  TableColumn,
+  TableRow,
+  TableCell,
+  getKeyValue,
+
+  Navbar, 
+  NavbarBrand, 
+  NavbarContent, 
+  NavbarItem, 
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem
+} from "@nextui-org/react";
+
 export default function Dashboard() {
+  const rows = [
+    {
+      key: "1",
+      category: "rent",
+      payment: "rent",
+      amount: -950,
+    },
+  ];
+
+  const columns = [
+    {
+      key: "category",
+      label: "Category",
+    },
+    {
+      key: "payment",
+      label: "Payment",
+    },
+    {
+      key: "amount",
+      label: "Amount",
+    },
+  ];
+
   return (
     <div>
-      <p>This will be the dashboard.</p>
-      <p>Find me at src/app/dashboard/page.tsx</p>
+      <Navbar>Finesse</Navbar>
+      <div>
+        <div>
+          <div>
+            <div>Bar</div>
+            <div>
+              <b>income</b>
+              <br />
+              <b>Expences</b>
+            </div>
+          </div>
+          <Table aria-label="Example table with dynamic content">
+            <TableHeader>
+              {columns.map((column) => (
+                <TableColumn key={column.key}>{column.label}</TableColumn>
+              ))}
+            </TableHeader>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow key={row.key}>
+                  {(columnKey) => (
+                    <TableCell>{getKeyValue(row, columnKey)}</TableCell>
+                  )}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+        <div>
+          <p>Chart</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
