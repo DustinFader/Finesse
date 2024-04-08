6c1bdd9dc23381897e889963bd8af98f33812607
DustinFader <dustinfader16@gmail.com>
2024-04-08 20:41:54 -0300

    add nav, footer, image and form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,23 @@
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
+import {Image} from "@nextui-org/react";
+
 export default function Login() {
   return (
-    <div>
-      <p>This will be the login.</p>
-      <p>Find me at src/app/login/page.tsx</p>
+    <div className="dark:bg-teal-800 flex flex-col min-h-screen">
+      <Header/>
+      <main className="flex justify-evenly flex-grow items-center">
+        <Image width={500} alt="blah" src="/image/blah.png"/>
+        <form className="flex flex-col bg-gray-300">
+          <h1>Welcome Back.</h1>
+          <label>Email Address</label>
+          <input placeholder="Email Address"></input>
+          <label>Password</label>
+          <input placeholder="Password"></input>
+          <button>Login</button>
+        </form>
+      </main>
+      <Footer/>
     </div>
   )
 }
