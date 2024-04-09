9eab745c72b998601abe8f6a7caefd6f2e87556e
M-ARTHELL <makoarthell@proton.me>
2024-04-09 17:23:41 -0300

    add category creation

--- src/app/api/payments/route.tsx
+++ src/app/api/payments/route.tsx
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server'
 import { PrismaClient } from "@prisma/client";
+import { CategoryScale } from 'chart.js';
 const prisma = new PrismaClient();
 
 export async function GET(request: Request) {
@@ -9,35 +10,59 @@ export async function GET(request: Request) {
 
 export async function POST(request: Request) {
   const body = await request.formData();
-  let additive_bool = null;
-  let amountInt = parseInt(body.get("amount"));
-  console.log(body)
-
-  if (body.get("is_additive")) {
-    additive_bool = true;
-    console.log(additive_bool)
-  } else {
-    additive_bool = false;
-    console.log(additive_bool)
-  }
-
-  await prisma.payments.create({
-    data: {
-      user_id: 7,
-      category_id: 1,
-      name: body.get("payment_name"),
-      amount: amountInt,
-      is_additive: additive_bool,
+  let category = await prisma.categories.findFirst({
+    where: {
+      name: body.get("category"),
     }
   })
 
-  const showPayment = await prisma.payments.findMany({
-    where: {
-      name: body.get("payment_name"),
-    },
-  })
+  let additive_bool = null;
+  let amountInt = parseInt(body.get("amount"));
+  console.log(body)
+  console.log(category)
 
-  console.log(showPayment)
+  if (body.get("is_additive")) {
+    additive_bool = true;
+  } else {
+    additive_bool = false;
+  }
+
+  //see if category exists
+  if (!category) {
+
+    //if no category, create it
+    const category = await prisma.categories.create({
+      data: {
+        name: body.get("category"),
+      }
+    })
+
+    // create payment with category id
+    const newPayment = await prisma.payments.create({
+      data: {
+        user_id: 7,
+        category_id: category.category_id,
+        name: body.get("payment_name"),
+        amount: amountInt,
+        is_additive: additive_bool,
+      }
+    })
+
+    category.payment_id = newPayment.payment_id;
+    
+  } else {
+    
+    // create payment with category id
+    await prisma.payments.create({
+      data: {
+        user_id: 7,
+        category_id: category.category_id,
+        name: body.get("payment_name"),
+        amount: amountInt,
+        is_additive: additive_bool,
+      }
+    })
+  }
 
   return NextResponse.json({message: "passing", body: body});
 }
