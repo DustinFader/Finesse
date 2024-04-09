2e1c5f1639e936bb7249881ddc76914159651f07
DustinFader <dustinfader16@gmail.com>
2024-04-08 21:52:39 -0300

    add useState use for future use.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,21 +1,51 @@
-import Header from "@/components/Header"
-import Footer from "@/components/Footer"
-import {Image, Button, Input} from "@nextui-org/react";
+'use client'
+
+import React, { useState } from "react";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { Image, Button, Input } from "@nextui-org/react";
 
 export default function Login() {
+  const [inputValue1, setValue1] = useState("");
+  const [inputValue2, setValue2] = useState("");
+
   return (
     <div className="dark:bg-teal-800 flex flex-col min-h-screen">
-      <Header/>
+      <Header />
       <main className="flex justify-evenly flex-grow items-center">
-        <Image width={500} alt="blah" src="/image/blah.png"/>
+        <Image width={500} alt="blah" src="/image/blah.png" />
         <form className="flex flex-col bg-gray-300 text-2xl p-44 space-y-9">
           <h1 className="text-4xl font-semibold">Welcome Back!</h1>
-          <Input classNames={{label: "text-black/100 dark:text-white/90"}} color="secondary" size="lg" type="email" label="Email Address" labelPlacement="outside" placeholder="Input Your Email" isRequired isClearable/>
-          <Input classNames={{label: "text-black/100 dark:text-white/90"}} color="secondary" size="lg" type="email" label="Password" labelPlacement="outside" placeholder="Input Your Password" isRequired isClearable/>
+          <Input
+            value={inputValue1}
+            onValueChange={setValue1}
+            classNames={{ label: "text-black/100 dark:text-white/90" }}
+            color="secondary"
+            size="lg"
+            type="email"
+            label="Email Address"
+            labelPlacement="outside"
+            placeholder="Input Your Email"
+            isRequired
+            isClearable
+          />
+          <Input
+            value={inputValue2}
+            onValueChange={setValue2}
+            classNames={{ label: "text-black/100 dark:text-white/90" }}
+            color="secondary"
+            size="lg"
+            type="password"
+            label="Password"
+            labelPlacement="outside"
+            placeholder="Input Your Password"
+            isRequired
+            isClearable
+          />
           <Button color="primary">Login</Button>
         </form>
       </main>
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );
 }
