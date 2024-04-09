f83733d6e6ae778fd0da5b246990532812524575
DustinFader <dustinfader16@gmail.com>
2024-04-08 21:39:12 -0300

    add a button, input and styling

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,6 @@
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
-import {Image} from "@nextui-org/react";
+import {Image, Button, Input} from "@nextui-org/react";
 
 export default function Login() {
   return (
@@ -8,13 +8,11 @@ export default function Login() {
       <Header/>
       <main className="flex justify-evenly flex-grow items-center">
         <Image width={500} alt="blah" src="/image/blah.png"/>
-        <form className="flex flex-col bg-gray-300">
-          <h1>Welcome Back.</h1>
-          <label>Email Address</label>
-          <input placeholder="Email Address"></input>
-          <label>Password</label>
-          <input placeholder="Password"></input>
-          <button>Login</button>
+        <form className="flex flex-col bg-gray-300 text-2xl p-44 space-y-9">
+          <h1 className="text-4xl font-semibold">Welcome Back!</h1>
+          <Input classNames={{label: "text-black/100 dark:text-white/90"}} color="secondary" size="lg" type="email" label="Email Address" labelPlacement="outside" placeholder="Input Your Email" isRequired isClearable/>
+          <Input classNames={{label: "text-black/100 dark:text-white/90"}} color="secondary" size="lg" type="email" label="Password" labelPlacement="outside" placeholder="Input Your Password" isRequired isClearable/>
+          <Button color="primary">Login</Button>
         </form>
       </main>
       <Footer/>
