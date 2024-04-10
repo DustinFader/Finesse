984c8df435c8001c5b1c3fc1a7d22be021adecf4
DustinFader <dustinfader16@gmail.com>
2024-04-10 18:20:28 -0300

    remove footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,6 @@ import React from "react"
 
 export default function Footer() {
   return (
-    <footer className="flex justify-center h-20 bg-gray-700">footer</footer>
+    <footer className="flex justify-center h-20 bg-gray-700"></footer>
   )
 }
