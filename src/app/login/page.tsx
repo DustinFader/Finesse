12c8c55bbf49bcf691fa54f6da51b2455b38f1f2
M-ARTHELL <makoarthell@proton.me>
2024-04-11 20:50:22 -0300

    style dashboard + login pages

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,24 @@
+import React from "react";
+import Header from "@/components/Header"
+import {Button, Input} from "@nextui-org/react";
+
 export default function Login() {
   return (
-    <div>
-      <p>This will be the login.</p>
-      <p>Find me at src/app/login/page.tsx</p>
+    <div className="dark:bg-blue-900 flex flex-col min-h-screen">
+      <Header />
+      <main className="flex justify-center mt-10">
+        <div className="w-1/2 bg-blue-800 p-10 rounded-lg">
+          <h1 className="text-3xl mb-4 font-semibold">Welcome Back...</h1>
+          <form action="/api/users" method="GET" id="addUser">
+            <Input name="email" type="email" id="email" label="Email" className="m-4" color="primary"/>
+            <Input name="password" type="password" id="password" label="Password" className="m-4" color="primary"/>
+          </form>
+
+          <Button type="submit" form="getUser" value="Submit" className="ml-4 bg-amber-700">
+            Login
+          </Button>
+        </div>
+      </main>
     </div>
   )
 }
