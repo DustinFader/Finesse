88ac5a7e04e4da53c0245e9a399470cb95a1fd8c
DustinFader <dustinfader16@gmail.com>
2024-04-12 18:09:54 -0300

    have form use post and move button inside from

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,14 +9,13 @@ export default function Login() {
       <main className="flex justify-center mt-10">
         <div className="w-1/2 bg-blue-800 p-10 rounded-lg">
           <h1 className="text-3xl mb-4 font-semibold">Welcome Back...</h1>
-          <form action="/api/users" method="GET" id="addUser">
+          <form action="/api/users" method="POST" id="getUser">
             <Input name="email" type="email" id="email" label="Email" className="m-4" color="primary"/>
             <Input name="password" type="password" id="password" label="Password" className="m-4" color="primary"/>
+            <Button type="submit" form="getUser" value="Submit" className="ml-4 bg-amber-700">
+              Login
+            </Button>
           </form>
-
-          <Button type="submit" form="getUser" value="Submit" className="ml-4 bg-amber-700">
-            Login
-          </Button>
         </div>
       </main>
     </div>
