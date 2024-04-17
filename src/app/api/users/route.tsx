7734191c327842b1dfb173f479006a51d991f84d
M-ARTHELL <makoarthell@proton.me>
2024-04-17 19:43:27 -0300

    add cookie when logging in/creating user

--- src/app/api/users/route.tsx
+++ src/app/api/users/route.tsx
@@ -1,14 +1,23 @@
 import { NextResponse } from 'next/server'
 import prisma from '@/lib/prisma'
+import { cookies } from 'next/headers';
 
 export async function POST(request: Request) { 
   const body = await request.formData();
 
-  await prisma.users.create({
+  const loggedInUser = await prisma.users.create({
     data: {
       email: body.get("email"),
       password: body.get("password"),
     }
+  })
+
+  cookies().set({
+    name: "user",
+    value: 'test',
+    httpOnly: true,
+    path: '/',
+    maxAge: 3600,
   })
 
   return NextResponse.redirect(new URL('/', request.url));
