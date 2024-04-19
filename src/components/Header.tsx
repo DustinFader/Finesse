ff5a92e3c9ff7cf7176dbb95a9ee17fb08607eff
M-ARTHELL <makoarthell@proton.me>
2024-04-18 21:40:59 -0300

    add logout button, use cookie for login status

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+'use client'
+
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import {
   Navbar,
   NavbarBrand,
@@ -8,18 +11,34 @@ import {
   Button,
 } from "@nextui-org/react";
 
-const user = "billiam"
 
-export default function Header() {
+export default function Header( {user}) {
+
+  
+
+  const router = useRouter()
+
+  const handleClickLogout = () => {
+    localStorage.removeItem("user")
+    router.push("/login")
+  }
+
   return ( 
   <Navbar position="static" className="bg-yellow-500" maxWidth="full">
     <NavbarBrand className="justify-start"><Link href="/dashboard" className="font-bold text-3xl text-white">Finesse</Link></NavbarBrand>
     <NavbarContent justify="end">
       {user
         ?
+        <>
           <NavbarItem>
-          Logged in as: {user}
+            Logged in as: {user.email}
           </NavbarItem>
+          <NavbarItem>
+            <Button onClick={handleClickLogout} className="bg-amber-600">
+              Logout
+            </Button>
+          </NavbarItem>
+        </>
         :
         <>
           <NavbarItem>
