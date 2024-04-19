a291af6decac4fa5825539284d11fb0524df25c5
M-ARTHELL <makoarthell@proton.me>
2024-04-18 21:42:09 -0300

    set cookie when logging in

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,22 +1,47 @@
+'use client'
+
 import React from "react";
 import Header from "@/components/Header"
 import {Button, Input} from "@nextui-org/react";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
+  const router = useRouter();
+
+  const handleSubmitLogin = (e) => {
+    e.preventDefault()
+    const {email, password} = e.target.elements
+    const body = {
+      email: email.value,
+      password: password.value,
+    }
+    fetch("/api/users", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    })
+    .then(res => res.json())
+    .then(data => {
+      localStorage.setItem("user", JSON.stringify(data))
+      router.push("/dashboard")
+    })
+  }
   return (
     <div className="dark:bg-blue-900 flex flex-col min-h-screen">
       <Header />
       <main className="flex justify-center mt-10">
         <div className="w-1/2 bg-blue-800 p-10 rounded-lg">
           <h1 className="text-3xl mb-4 font-semibold">Welcome Back...</h1>
-          <form action="/api/users" method="POST" id="addUser">
+          <form onSubmit={handleSubmitLogin} id="loginForm">
             <Input name="email" type="email" id="email" label="Email" className="m-4" color="primary"/>
             <Input name="password" type="password" id="password" label="Password" className="m-4" color="primary"/>
-          </form>
 
-          <Button type="submit" form="addUser" value="Submit" className="ml-4 bg-amber-700">
+          <Button type="submit" form="loginForm" className="ml-4 bg-amber-700">
             Login
           </Button>
+          </form>
         </div>
       </main>
     </div>
