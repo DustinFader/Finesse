5112c3847c33e1987efe18f8eeaa995f083442a0
M-ARTHELL <makoarthell@proton.me>
2024-04-20 14:21:05 -0300

    brighten expense color, extend it to end of chart

--- src/components/FinanceTable.tsx
+++ src/components/FinanceTable.tsx
@@ -142,7 +142,6 @@ export default function FinanceTable({payments, setPayments, categories, setCate
                     >
                       Additive
                     </Checkbox>
-                    <p>Selected: {checkboxValue ? "true" : "false"}</p>
                   </div>
               </ModalBody>
               <ModalFooter>
@@ -170,8 +169,8 @@ export default function FinanceTable({payments, setPayments, categories, setCate
             <TableRow key={payment.payment_id}>
               {(columnKey) => {
                 return columnKey !== "delete"
-                ? <TableCell className={payment.is_additive ? "" : "bg-red-900"}>{getKeyValue(payment, columnKey)}</TableCell>
-                : <TableCell><Button onClick={() => handleClickDeletePayment(payment.payment_id)} className="bg-amber-600">Delete</Button></TableCell>
+                ? <TableCell className={payment.is_additive ? "" : "bg-red-700"}>{getKeyValue(payment, columnKey)}</TableCell>
+                : <TableCell className={payment.is_additive ? "" : "bg-red-700"}><Button onClick={() => handleClickDeletePayment(payment.payment_id)} className="bg-amber-600">Delete</Button></TableCell>
               }}
             </TableRow>
           )}
